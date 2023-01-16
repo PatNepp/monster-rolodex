21e14f182f17556b82788c1d94cc8656354fbe00
Patrick Nepp <ptnepp@gmail.com>
2023-01-16 16:14:43 -0600

    initial commit and search feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('didMount')
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return {monsters: users}
+      }))
+  }
+
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLowerCase()
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
+  render() {
+      console.log('render')
+      const {monsters, searchField} = this.state
+      const {onSearchChange} = this
+
+
+      const filteredMonsters = monsters.filter((monster) => {
+        return monster.name.toLowerCase().includes(searchField)
+      })
+
+      return (
+      <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='Search Monsters'
+          //best practice is to move anonymous functions out of onChange attribute and move to its own method/function
+              //WHY? because now the function won't run every time the app rerenders
+          onChange={onSearchChange} 
+      />
+        {filteredMonsters.map((monster, i) => {
+          return <h1 key={i}>{monster.name}</h1>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
