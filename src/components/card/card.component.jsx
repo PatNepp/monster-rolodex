908345057f64023a4e8efeb0907d000588a38ad5
Patrick Nepp <ptnepp@gmail.com>
2023-01-17 18:44:48 -0600

    convert components to functional components

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,19 +1,31 @@
-import { Component } from "react";
 import './card.styles.css'
 
-class Card extends Component {
-    render() {
-        const { id, name, email, address } = this.props.monster
-        
-        return (
-            <div className='card-container'>
-                <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`}/>
-                <h1>{name}</h1>
-                <h3>{email}</h3>
-                <h5>{`${address.street}, ${address.suite}, ${address.city}`}</h5>
-            </div>
-        )
-    }
+const Card = ({monster}) => {
+    const { id, name, email, address } = monster
+
+    return (
+        <div className='card-container'>
+            <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`}/>
+            <h1>{name}</h1>
+            <h3>{email}</h3>
+            <h5>{`${address.street}, ${address.suite}, ${address.city}`}</h5>
+        </div>
+    )    
 }
+
+// class Card extends Component {
+//     render() {
+//         const { id, name, email, address } = this.props.monster
+
+//         return (
+//             <div className='card-container'>
+//                 <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`}/>
+//                 <h1>{name}</h1>
+//                 <h3>{email}</h3>
+//                 <h5>{`${address.street}, ${address.suite}, ${address.city}`}</h5>
+//             </div>
+//         )
+//     }
+// }
 
 export default Card
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,21 +1,33 @@
-import { Component } from "react";
 import './search-box.styles.css'
 
-class SearchBox extends Component {
-    render() {
-        const {onChange, className, placeholder} = this.props
-
-        return(
-            <div>
-                <input 
-                    className={`search-box ${className}`} 
-                    type='search' 
-                    placeholder={placeholder}
-                    onChange={onChange} 
-                />
-            </div>
-        )
-    }
+const SearchBox = ({onChange, className, placeholder}) => {
+    return(
+        <div>
+            <input 
+                className={`search-box ${className}`} 
+                type='search' 
+                placeholder={placeholder}
+                onChange={onChange} 
+            />
+        </div>
+    )
 }
+
+// class SearchBox extends Component {
+//     render() {
+//         const {onChange, className, placeholder} = this.props
+
+//         return(
+//             <div>
+//                 <input 
+//                     className={`search-box ${className}`} 
+//                     type='search' 
+//                     placeholder={placeholder}
+//                     onChange={onChange} 
+//                 />
+//             </div>
+//         )
+//     }
+// }
 
 export default SearchBox
